e0dc8e41ed1bef33e48614d8117bf3cd4f0cfea8
Vik <valakar@gmail.com>
2020-11-01 19:39:10 +0200

    feat(product-service): Add GET /products endpoint

---
+++ product-service/serverless.ts
@@ -0,0 +1,43 @@
+import { Serverless } from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: 'product-service',
+  },
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  // Add the serverless-webpack plugin
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    region: 'eu-west-1',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+  },
+  functions: {
+    getProductList: {
+      handler: 'handler.getProductList',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'products',
+            cors: true
+          }
+        }
+      ]
+    }
+  }
+}
+
+module.exports = serverlessConfiguration;
