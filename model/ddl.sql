1c59251756798d9c16fe0d1da395982d1aaa2ea1
Vik <valakar@gmail.com>
2020-11-08 17:27:40 +0200

    feat(rds): Add initial ddl amd dml scripts

---
+++ model/ddl.sql
@@ -0,0 +1,17 @@
+create table product (
+    id uuid primary key default uuid_generate_v4(),
+    title varchar(256)  not null,
+    description text,
+    tier varchar(30),
+    image varchar(256),
+    price int,
+    score int
+)
+
+create table stock (
+    id uuid primary key default uuid_generate_v4(),
+    product_id uuid unique not null references product(id),
+    count int not null default 0
+)
+
+--create extension if not exists "uuid-ossp";
---
+++ model/seed.sql
@@ -0,0 +1,30 @@
+INSERT INTO product (id,title,description,tier,image,price,score) VALUES
+ ('a6a113b1-b3f3-4f45-abd2-3704c24530a3','Red Dead Redemption 2','Red Dead Redemption 2 is a game in Mighty tier with score of 95','Mighty','https://c.opencritic.com/images/games/3717/banner.jpg',192,95),
+ ('b0e24f1e-5cf3-4eac-8537-32499fc67a85','Grand Theft Auto V','Grand Theft Auto V is a game in Mighty tier with score of 98','Mighty','https://c.opencritic.com/images/games/163/banner.jpg',70,98),
+ ('b211af41-4d35-48e6-a12c-c0dd6015e4da','God of War','God of War is a game in Mighty tier with score of 98','Mighty','https://c.opencritic.com/images/games/5434/banner.jpg',175,98),
+ ('2871ee0f-ccdb-4ca2-9f41-575fd0cf4be1','Persona 5 Royal','Persona 5 Royal is a game in Mighty tier with score of 99','Mighty','https://c.opencritic.com/images/games/8785/GHStTWAVPE7hrSZ5pWdsJAUwhrxR7oO5itnKhAdZUVVRxHZl.jpg',105,99),
+ ('dbe5ff2c-e7cb-43f6-927e-aeba6d6fa9c9','The Last of Us Remastered','The Last of Us Remastered is a game in Mighty tier with score of 100','Mighty','https://c.opencritic.com/images/games/234/banner.jpg',56,100),
+ ('63887aeb-b0c4-48a1-befe-5de577c41686','Persona 5','Persona 5 is a game in Mighty tier with score of 99','Mighty','https://c.opencritic.com/images/games/1537/banner.jpg',126,99),
+ ('2492e805-471d-493e-b60d-dcba6f11128b','Super TIME Force Ultra','Super TIME Force Ultra is a game in Mighty tier with score of 80','Mighty','https://c.opencritic.com/images/games/958/banner.jpg',6,80),
+ ('c55c4837-7815-4de0-beef-3ef401390d6b','Undertale','Undertale is a game in Mighty tier with score of 96','Mighty','https://c.opencritic.com/images/games/1907/banner.jpg',60,96),
+ ('415717e6-55a2-4556-9d1e-d6232bc49ae1','Journey','Journey is a game in Mighty tier with score of 93','Mighty','https://c.opencritic.com/images/games/1436/banner.jpg',33,93),
+ ('440c4307-f20b-4ddd-a0e1-c5d83cf74919','The Last of Us Part II','The Last of Us Part II is a game in Mighty tier with score of 95','Mighty','https://c.opencritic.com/images/games/8351/banner.jpg',183,95),
+ ('c191e844-2ca2-4925-add3-1c8b51795ecf','flower','flower is a game in Mighty tier with score of 100','Mighty','https://c.opencritic.com/images/games/312/banner.jpg',6,100),
+ ('1c3c8705-3200-4fc2-9c4f-19b309b33232','Uncharted 4: A Thief''s End','Uncharted 4: A Thief''s End is a game in Mighty tier with score of 96','Mighty','https://c.opencritic.com/images/games/1538/banner.jpg',128,96),
+ ('d970ba72-4f5c-4b1f-a8ee-2dfd7e894546','Resident Evil 2','Resident Evil 2 is a game in Mighty tier with score of 97','Mighty','https://c.opencritic.com/images/games/6848/banner.jpg',185,97),
+ ('7cf74094-c7b8-4009-88fe-621a8072f765','Minecraft: PlayStation 4 Edition','Minecraft: PlayStation 4 Edition is a game in Mighty tier with score of 100','Mighty','https://c.opencritic.com/images/games/194/banner.jpg',7,100),
+ ('394ad313-97a4-4336-a29b-77a0287d3ba9','Shadow of the Colossus','Shadow of the Colossus is a game in Mighty tier with score of 95','Mighty','https://c.opencritic.com/images/games/5118/banner.jpg',127,95),
+ ('688e6e1c-aa30-4ffb-8e04-228feb2c3a4a','The Witcher 3: Wild Hunt - Blood and Wine','The Witcher 3: Wild Hunt - Blood and Wine is a game in Mighty tier with score of 98','Mighty','https://c.opencritic.com/images/games/2704/banner.jpg',62,98),
+ ('6fa0f339-02d5-426d-a863-955d8fa38ee0','The Witcher 3: Wild Hunt','The Witcher 3: Wild Hunt is a game in Mighty tier with score of 95','Mighty','https://c.opencritic.com/images/games/463/banner.jpg',116,95),
+ ('14a5f707-132f-4e12-bdb2-44a6df173129','Metal Gear Solid V: The Phantom Pain','Metal Gear Solid V: The Phantom Pain is a game in Mighty tier with score of 98','Mighty','https://c.opencritic.com/images/games/1887/banner.jpg',90,98),
+ ('19c6a1aa-674b-4c3a-9890-aa7f4c6b3eed','Celeste','Celeste is a game in Mighty tier with score of 100','Mighty','https://c.opencritic.com/images/games/5403/banner.jpg',69,100),
+ ('cdb3b1a4-2c22-4563-98a5-664391ae2bba','Divinity: Original Sin 2 – Definitive Edition','Divinity: Original Sin 2 – Definitive Edition is a game in Mighty tier with score of 98','Mighty','https://c.opencritic.com/images/games/6529/banner.jpg',60,98),
+ ('df871df6-80e5-4df5-b402-85b045fa66a9','Dota 2','Dota 2 game desc','Mighty','https//c.opencritic.com/images/games/9753/RF8gbUQXdnmCLvScpLMuP3r8tjm6QoKAdMSJx3T5FwLr1WH9.jpg',4,94);
+
+insert into stock (product_id, count) values
+('a6a113b1-b3f3-4f45-abd2-3704c24530a3', 10),
+('b0e24f1e-5cf3-4eac-8537-32499fc67a85', 8),
+('b211af41-4d35-48e6-a12c-c0dd6015e4da', 5),
+('2871ee0f-ccdb-4ca2-9f41-575fd0cf4be1', 3),
+('df871df6-80e5-4df5-b402-85b045fa66a9', 999);
+
