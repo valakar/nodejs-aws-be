7f31cedc181097be0637a775ec7be3501922018d
Vik <valakar@gmail.com>
2020-11-17 08:53:53 +0200

    fix: Add proper type for importFileParser lambda

--- shared/utility/logger.ts
+++ shared/utility/logger.ts
@@ -1,11 +1,5 @@
-interface APIGatewayProxyEvent {
-    body: string | null;
-    queryStringParameters: { [name: string]: string } | null;
-    pathParameters: { [name: string]: string } | null;
-}
-
 export namespace Logger {
-    export const logEvent = (event: APIGatewayProxyEvent): void => {
+    export const logEvent = (event): void => {
         console.log(`Event: ${JSON.stringify(event)}`);
         console.log(`Body: ${JSON.stringify(event?.body)}`);
         console.log(`QueryParams: ${JSON.stringify(event?.queryStringParameters)}`);
